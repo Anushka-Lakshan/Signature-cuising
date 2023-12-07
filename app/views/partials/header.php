6a76cd96099859f6317b97eef313e9c5bc45e0d7
Anushka lakshan <anushkalakshan1996@gmail.com>
2023-12-07 21:42:12 +0530

    Favicon added

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -25,6 +25,12 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="./assets/js/jquery.min.js"></script>
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/Favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/Favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/Favicon/favicon-16x16.png">
+    <link rel="manifest" href="./assets/Favicon/site.webmanifest">
+
 </head>
 
 <body>
